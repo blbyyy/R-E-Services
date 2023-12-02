402a69afcf1f85f286bc7454dbf7c2398aa10ac9
Joseph Andre Balbada <josephandrebalbada23@gmail.com>
2023-12-02 20:22:51 +0800

    R&E-Services

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -56,6 +56,7 @@ class AdminController extends Controller
         return View::make('admin.dashboard',compact('usersCount','studentCount','staffCount','facultyCount','applicationCount','admin','pendingCount','passedCount','returnedCount'));
     }
 
+    //show comments
     public function showannouncement()
     {
         $student = DB::table('students')
