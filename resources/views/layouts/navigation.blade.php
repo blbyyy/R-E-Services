d7e5e992e10a2ddba28c2bd8a497f9ac8ef7a57d
Joseph Andre Balbada <josephandrebalbada23@gmail.com>
2024-01-19 02:11:06 +0800

    tryyyy

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,8 +20,19 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   
     <!-- Vendor CSS Files -->
-    <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/bootstrap/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/bootstrap-icons/bootstrap-icons.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/boxicons/css/boxicons.min.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/quill/quill.snow.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/quill/quill.bubble.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/remixicon/remixicon.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/vendor/simple-datatables/style.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/assets/css/style.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/css/login.css">
+   <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/css/comment.css">
+
+    
+    {{-- <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
@@ -29,7 +40,7 @@
     <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/comment.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/comment.css') }}" rel="stylesheet"> --}}
   
   </head>
   
