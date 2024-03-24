5f9c646bf45daaf06f47da94f5228016068d3373
Joseph Andre Balbada <josephandrebalbada23@gmail.com>
2024-03-25 02:38:15 +0800

    revision

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,16 +35,16 @@
    <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/css/login.css">
    <link rel="stylesheet" href="https://redigitalize-production.up.railway.app/css/comment.css"> --}}
     
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/comment.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../css/login.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('../css/comment.css') }}" rel="stylesheet">
   
   </head>
   
