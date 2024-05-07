ce0306581dc33ab92f7691156c4768b4d9807390
Joseph Andre Balbada <josephandrebalbada23@gmail.com>
2024-05-08 01:36:39 +0800

    erg

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>R&E-Services</title>
-    <link rel="website icon" type="png" href="{{ asset('assets/img/RED.png')}}">
+    <link rel="website icon" type="png" href="{{ secure_asset('../assets/img/RED.png')}}">
 
     <meta content="" name="description">
     <meta content="" name="keywords">
